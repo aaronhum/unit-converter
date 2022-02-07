78772081dfbb716da69832a482b9d29f1c7d5bb9
Aaron Hum <aaronchum1@gmail.com>
2022-02-06 18:53:53 -0700

    added carbon themes

--- src/components/header-bar/HeaderBar.js
+++ src/components/header-bar/HeaderBar.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { Header, HeaderName } from "carbon-components-react";
+import {
+  Header,
+  HeaderName,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
+} from "carbon-components-react";
+import { Code32 } from "@carbon/icons-react";
 import "./HeaderBar.scss";
+
+// document.documentElement.setAttribute("carbon-theme", theme); // "white" | "g10" | "g90" | "g100"
 
 const HeaderBar = () => {
   return (
     <div>
       <Header aria-label="Header">
         <HeaderName prefix="Unit">Converter</HeaderName>
+        <HeaderGlobalBar>
+          <HeaderGlobalAction
+            aria-label="See Source Code"
+            href="https://github.com/aaronhum/unit-converter"
+          >
+            <Code32 />
+          </HeaderGlobalAction>
+        </HeaderGlobalBar>
       </Header>
     </div>
   );
